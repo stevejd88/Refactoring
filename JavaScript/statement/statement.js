27ecded3c015c9cc900b6a5efc8891ba852afca2
Steven De Avila <stevejd88@gmail.com>
2022-01-11 16:58:37 -0600

    Rename variables in volumeCreditsFor function

--- JavaScript/statement/statement.js
+++ JavaScript/statement/statement.js
@@ -38,14 +38,15 @@ const statement = (invoice, plays) => {
     return result;
   }
 
-  // EXTRACT VOLUME CREDITS LOGIC
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
+  // 1. EXTRACT VOLUME CREDITS LOGIC
+  // 2. RENAME VARIABLES
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 
   for (let perf of invoice.performances) {
